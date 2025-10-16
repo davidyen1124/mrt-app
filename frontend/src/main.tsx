e0cb4981d4b0b650dbae7cc31eba3ccb0f65314c
David Yen <davidyen1124@gmail.com>
2025-10-16 05:52:07 -0700

    fix(frontend): bundle maplibre css

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,4 @@
+import 'maplibre-gl/dist/maplibre-gl.css'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './ui/App'
@@ -8,4 +9,3 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>
 )
-
